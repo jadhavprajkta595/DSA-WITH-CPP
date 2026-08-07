20ec466a981fe4292233bcdd64a7fdd4a52c7c06
Prajkta Jadhav <jadhavprajkta595@gmail.com>
2026-08-07 17:34:30 +0530

    modified starting of DSA

---
+++ Cpp basics/Multiple_of_10.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+int n;
+do{
+    cout<<"enter number :";
+    cin>>n;
+    if(n % 10 == 0){
+        break;
+    }
+    cout<<"you entered :"<< n <<endl;
+}while(true);
+return 0;
+}
