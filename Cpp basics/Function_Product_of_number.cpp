15c5a766f23e63be44e03967154ffa0a2b0c75cb
Prajkta Jadhav <jadhavprajkta595@gmail.com>
2026-08-17 14:34:39 +0530

    modified starting of DSA

---
+++ Cpp basics/Function_Product_of_number.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int product(int a,int b){
+int product = a * b;
+return product ;
+}
+int main(){
+int p= product(5,5);
+cout<<"product = " << p <<endl;
+return 0;
+
+
+}
